e573c52ea431623d191d598e7d652d9e2d583dff
angeleno <sarang.baheti@gmail.com>
2017-01-10 00:24:20 -0800

    systemstats app with livecharts

--- livechart2/Properties/AssemblyInfo.cs
+++ livechart2/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("livechart2")]
+[assembly: AssemblyTitle("systemstats")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("livechart2")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("systemstats")]
+[assembly: AssemblyCopyright("Copyright Sarang Baheti ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
